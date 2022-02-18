d64c427d39c227f25809c3e49c73d7043ae01940
Gem Eliezer <gemeliezerespiritu@gmail.com>
2022-02-18 13:26:40 +0800

    pwa offline

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = [ 'index.html', 'offline.html' ];
+// const urlsToCache = [ 'index.html', 'offline.html' ];
+let urlsToCache = ["/"]
 
 const self = this;
 
