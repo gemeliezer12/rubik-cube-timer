5e86478484a27a5726dc92d828ac77ef2aad7c67
Gem Eliezer <gemeliezerespiritu@gmail.com>
2022-02-18 13:02:08 +0800

    trying to make wev app work offline

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import App from './App'
+
 import './styles/index.css'
 import "./styles/style.css"
-import App from './App'
 
 ReactDOM.render(
     <React.StrictMode>
