bf090469e910fd06c249f868782e16e17417b1cd
Gem <gemeliezerespiritu@gmail.com>
2022-03-15 08:57:29 +0800

    bottom bar

--- src/Components/BottomBar/index.js
+++ src/Components/BottomBar/index.js
@@ -2,19 +2,22 @@ const Index = () => {
     return (
         <div style={{
             width: "100%",
-            height: "70px",
-            minHeight: "70px",
+            height: "60px",
+            minHeight: "60px",
             backgroundColor: "#4338ca",
             display: "flex",
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)",
             justifyContent: "space-around",
-            alignItems: "center",
+            // alignItems: "center",
+            padding: "5px",
             borderRadius: "32px",
             paddingLeft: "32px",
             paddingRight: "32px"
         }}>
             <div style={{
-                width: "50px",
-                height: "50px",
+                // width: "50px",
+                // height: "50px",
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
@@ -25,13 +28,13 @@ const Index = () => {
                     fontSize: "20px",
                 }}></i>
             </div>
-            <div style={{
-                width: "50px",
-                height: "50px",
+            <div className="light" style={{
+                // width: "50px",
+                // height: "50px",
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
-                backgroundColor: "white",
+                backgroundColor: "var(--bg-color-2)",
                 borderRadius: "200px",
                 cursor: "pointer"
             }}>
@@ -41,8 +44,8 @@ const Index = () => {
                 }}></i>
             </div>
             <div style={{
-                width: "50px",
-                height: "50px",
+                // width: "50px",
+                // height: "50px",
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
